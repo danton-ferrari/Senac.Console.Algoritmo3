cbd3e7225b8944c84793c2c69d8f1f6f28939065
DANTONFERRARI <dgabrielferrari7@gmail.com>
2025-03-27 21:24:41 -0300

    Código atualizado do exercicio da aula 27/03/2025

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System.Globalization;
 
 Console.Write("Informe seu nome completo: ");
-var nomeCompleto = Console.ReadLine();
+string nomeCompleto = Console.ReadLine() ?? "";
 Console.WriteLine(nomeCompleto);
 Console.WriteLine("");
 
 Console.Write("Quantos quartos tem na sua casa? ");
-var qtdeQuartos = Console.ReadLine();
+int qtdeQuartos = int.Parse(Console.ReadLine());
 Console.WriteLine($"Você tem {qtdeQuartos} quartos ");
 Console.WriteLine("");
 
@@ -16,9 +16,9 @@ Console.WriteLine($"O preço da sua internet é R${precoInternet:F2}");
 Console.WriteLine("");
 
 Console.Write("Informe seu ultimo nome, idade e altura na mesma linha: ");
-var ultimoNomeIdadeAltura = Console.ReadLine().Split(' ');
-var ultimoNome = ultimoNomeIdadeAltura[0];
-var idade = int.Parse(ultimoNomeIdadeAltura[1]);
+string[] ultimoNomeIdadeAltura = Console.ReadLine().Split(' ');
+string ultimoNome = ultimoNomeIdadeAltura[0];
+int idade = int.Parse(ultimoNomeIdadeAltura[1]);
 double altura = double.Parse(ultimoNomeIdadeAltura[2], CultureInfo.InvariantCulture);
 Console.WriteLine($"Seu ultimo nome é {ultimoNome}");
 Console.WriteLine($"Sua idade é {idade} anos");
